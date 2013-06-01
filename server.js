2811410bddc6aa166f0581b208771a8751692836
José Carlos Cuevas <reset.reboot@gmail.com>
2013-06-01 20:07:08 +0200

    First css tests.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.render('index',
-        { title: 'Home' }
+        { title: 'My Game' }
         );
 });
 
